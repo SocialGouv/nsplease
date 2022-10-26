7108b3f7863e1bb575bfb7d6d7d8421f5e3841bc
Matéo Mévollon <m.mevollon@protonmail.com>
2022-10-26 19:09:59 +0200

    fix: comments

--- nsplease.sh
+++ nsplease.sh
@@ -8,6 +8,8 @@ out() {
 
 main() {
   echo "waiting for 'nsplease/project' labelled namespaces..."
+
+  # watch namespace creations, only ADDED type and labelled with nsplease/project
   kubectl get namespace --watch --output-watch-events -o json |
     jq --unbuffered --raw-output \
       'if (.object.metadata.labels | has("nsplease/project"))
@@ -18,10 +20,12 @@ main() {
     while read -r NAMESPACE PROJECT; do
       echo "got labelled namespace: $NAMESPACE $PROJECT"
 
+      # give rights on namespace to project's ServiceAccount
       out kubectl create clusterrolebinding "nsplease-crb-$PROJECT-$NAMESPACE" \
         --clusterrole=cluster-admin \
         --serviceaccount="$PROJECT:nsplease-sa"
 
+      # remove label to avoid doing this again for the same namespace
       out kubectl label namespace "$NAMESPACE" nsplease/project-
     done
 }
